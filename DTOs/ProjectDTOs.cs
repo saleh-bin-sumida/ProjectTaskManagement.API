d21cd87e80d9f6c52bf7a033d622b21a0102eee5
Saleh Mohammed <salhbnsmyd3@gmail.com>
2025-05-01 13:51:31 +0300

    add project dto

---
+++ DTOs/ProjectDTOs.cs
@@ -0,0 +1,23 @@
+﻿namespace ProjectTaskManagement.API.DTOs;
+
+public class GetProjectDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime DateCreated { get; set; }
+
+}
+
+public class AddProjectDto
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
+
+public class UpdateProjectDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
