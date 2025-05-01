537a0c6e9974f709da077eb2e397a7f509dfd97f
Saleh Mohammed <salhbnsmyd3@gmail.com>
2025-05-01 13:57:48 +0300

    add task assingmet dto

---
+++ DTOs/TaskAssignmentDTOs.cs
@@ -0,0 +1,16 @@
+﻿namespace ProjectTaskManagement.API.DTOs;
+
+public class GetTaskAssignmentDto
+{
+    public int Id { get; set; }
+    public DateTime Date { get; set; }
+    public string UserName { get; set; }
+    public string TaskName { get; set; }
+}
+
+public class AddTaskAssignmentDto
+{
+    public int UserId { get; set; }
+    public int TaskId { get; set; }
+}
+
