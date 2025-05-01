06f4c1411461bfc90aaf7174f4261db7118f3aa7
Saleh Mohammed <salhbnsmyd3@gmail.com>
2025-05-01 13:51:15 +0300

    add project entity

---
+++ Models/Project.cs
@@ -0,0 +1,19 @@
+﻿namespace ProjectTaskManagement.API.Models;
+
+public class Project
+{
+    #region Keys
+    public int Id { get; set; }
+    #endregion
+
+    #region Scalar Properties
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime DateCreated { get; set; }
+    #endregion
+
+    #region Collections
+    public ICollection<Task> Tasks = [];
+    #endregion
+
+}
