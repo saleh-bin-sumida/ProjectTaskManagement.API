2dfac4401e1e61a5c0be0997b71d56f302465ecf
Saleh Mohammed <salhbnsmyd3@gmail.com>
2025-05-01 13:57:40 +0300

    add commet dto

---
+++ DTOs/CommentDTOs.cs
@@ -0,0 +1,22 @@
+﻿namespace ProjectTaskManagement.API.DTOs;
+
+public class GetCommentDto
+{
+    public int Id { get; set; }
+    public string Message { get; set; }
+    public string UserName { get; set; }
+    public string TaskName { get; set; }
+    public DateTime DateCreated { get; set; }
+
+}
+
+public class AddCommentDto
+{
+    public string Message { get; set; }
+    public int TaskAssignmentId { get; set; }
+}
+
+public class UpdateCommentDto
+{
+    public string Message { get; set; }
+}
