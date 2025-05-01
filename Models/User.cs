8522c17eb5fd81a4e9aaea5c7582040ab1465c44
Saleh Mohammed <salhbnsmyd3@gmail.com>
2025-05-01 13:54:34 +0300

    add user entity

---
+++ Models/User.cs
@@ -0,0 +1,20 @@
+﻿namespace ProjectTaskManagement.API.Models;
+
+public class User
+{
+    #region Keys
+    public int Id { get; set; }
+    #endregion
+
+    #region Scalar Properties
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public DateTime DateCreated { get; set; }
+    #endregion
+
+    #region Collections
+    public ICollection<Task>? TasksCreated = [];
+    public ICollection<TaskAssignment> TaskAssignments = [];
+    #endregion
+}
