41ae68e96f8ecfc22078b1d5531e995140b5c9f8
Saleh Mohammed <salhbnsmyd3@gmail.com>
2025-05-01 13:57:04 +0300

    add task status history entity

---
+++ Models/TaskStatusHistory.cs
@@ -0,0 +1,23 @@
+﻿namespace ProjectTaskManagement.API.Models;
+
+public class TaskStatusHistory
+{
+    #region Keys
+    public int Id { get; set; }
+    public int StatusId { get; set; }
+    public int TaskAssignmentId { get; set; }
+    #endregion
+
+    #region Navigations
+    public TaskStatus Status { get; set; }
+    public TaskAssignment TaskAssignment { get; set; }
+    #endregion
+
+    public DateTime Date { get; set; }
+
+    public TaskStatusHistory(int statusId, int taskAssignmentId)
+    {
+        this.StatusId = statusId;
+        this.TaskAssignmentId = taskAssignmentId;
+    }
+}
