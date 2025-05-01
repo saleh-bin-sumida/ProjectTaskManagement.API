6c09dfb5b2998a6ba259a64fdbbad28233dbafd0
Saleh Mohammed <salhbnsmyd3@gmail.com>
2025-05-01 13:50:10 +0300

    Set up ASP.NET Core project with OpenAPI support
    Updated .gitignore to include CodeCoverage/ directory.
    Configured `Program.cs` for ASP.NET Core web app with OpenAPI,
    database context, and middleware. Updated project file to target
    .NET 9.0 and added necessary package references. Created
    user-specific project settings file and updated solution file.
    Added `appsettings.json` for database connection strings and
    logging configuration.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+using Swashbuckle.AspNetCore.SwaggerUI;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.EnableSensitiveDataLogging();
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
+{
+    app.MapOpenApi();
+    app.UseSwaggerUI(optons =>
+    {
+        optons.DocExpansion(DocExpansion.None);
+        optons.SwaggerEndpoint("/openapi/v1.json", "v1");
+    });
+
+}
+
+
+app.Use(async (context, next) =>
+{
+    if (context.Request.Path == "/")
+    {
+        context.Response.Redirect("/swagger");
+        return;
+    }
+    await next();
+});
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
