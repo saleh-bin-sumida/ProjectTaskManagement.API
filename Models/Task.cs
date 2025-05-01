36e8ba2dcccb09b0ddf1e1b898452b0b85a0ad39
Saleh Mohammed <salhbnsmyd3@gmail.com>
2025-05-01 13:54:51 +0300

    add task entity

---
+++ Models/Task.cs
@@ -0,0 +1,27 @@
+﻿namespace ProjectTaskManagement.API.Models;
+
+public class Task
+{
+    #region Keys
+    public int Id { get; set; }
+    public int? StatusId { get; set; }
+    public int ProjectId { get; set; }
+    public int? CreatedByUserId { get; set; }
+    #endregion
+
+    #region Scalar Properties
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime DateCreated { get; set; }
+    #endregion
+
+    #region Navigations
+    public User CreatedByUser { get; set; }
+    public TaskStatus Status { get; set; }
+    public Project Project { get; set; }
+    #endregion
+
+    #region Collections
+    public ICollection<TaskAssignment> TaskAssignments = [];
+    #endregion
+}
