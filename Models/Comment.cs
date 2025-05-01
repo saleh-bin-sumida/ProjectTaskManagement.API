3bd7e43e841d1b9a66b88c668a59b822db77ab6d
Saleh Mohammed <salhbnsmyd3@gmail.com>
2025-05-01 13:56:50 +0300

    add comment entity

---
+++ Models/Comment.cs
@@ -0,0 +1,18 @@
+﻿namespace ProjectTaskManagement.API.Models;
+
+public class Comment
+{
+    #region Keys
+    public int Id { get; set; }
+    public int TaskAssignmentId { get; set; }
+    #endregion
+
+    #region Scalar Properties
+    public string Message { get; set; }
+    public DateTime DateCreated { get; set; }
+    #endregion
+
+    #region Navigations
+    public TaskAssignment TaskAssignment { get; set; }
+    #endregion
+}
