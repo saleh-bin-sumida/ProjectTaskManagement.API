04392847982574323e2e253da9b7679e4ca81dde
Saleh Mohammed <salhbnsmyd3@gmail.com>
2025-05-01 13:55:17 +0300

    add task status entity

---
+++ Models/TaskStatus.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ProjectTaskManagement.API.Models;
+
+public class TaskStatus
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    [JsonIgnore]
+    public ICollection<Task>? Tasks { get; set; }
+}
