13bdbfc5167ec8c9a7af8f44d1bb0e2f36f25b1a
Saleh Mohammed <salhbnsmyd3@gmail.com>
2025-05-01 13:57:23 +0300

    add user dto

---
+++ DTOs/UserDTOs.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjectTaskManagement.API.DTOs;
+
+public class GetUserDto
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public DateTime DateCreated { get; set; }
+
+}
+
+
+public class AddUserDto
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    [EmailAddress(ErrorMessage = "invalid email address")]
+    public string Email { get; set; }
+}
+
+
+public class UpdateUserDto
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+
+
