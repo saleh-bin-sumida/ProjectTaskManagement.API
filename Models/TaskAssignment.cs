435ead8add9e14e58dfd710e7f8e0089245e86f8
Saleh Mohammed <salhbnsmyd3@gmail.com>
2025-05-01 13:55:07 +0300

    add task assigment entity

---
+++ Models/TaskAssignment.cs
@@ -0,0 +1,24 @@
+﻿namespace ProjectTaskManagement.API.Models;
+
+public class TaskAssignment
+{
+    #region Keys
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public int TaskId { get; set; }
+    #endregion
+
+    #region Scalar Properties
+    public DateTime Date { get; init; }
+    #endregion
+
+    #region Navigations
+    public User User { get; set; }
+    public Task Task { get; set; }
+    #endregion
+
+    #region Collections
+    public ICollection<Comment> Comments = [];
+    public ICollection<TaskStatusHistory> TaskStatusHistories = [];
+    #endregion
+}
