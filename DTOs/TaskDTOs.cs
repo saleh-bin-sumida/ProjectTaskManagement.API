ec01b395e957caada2d41ac20fdf37818ec3dc8c
Saleh Mohammed <salhbnsmyd3@gmail.com>
2025-05-01 13:57:56 +0300

    add task dto

---
+++ DTOs/TaskDTOs.cs
@@ -0,0 +1,34 @@
+﻿namespace ProjectTaskManagement.API.DTOs;
+
+public class GetTaskDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string ProjectName { get; set; }
+    public string Status { get; set; }
+    public DateTime DateCreated { get; set; }
+    public string CreatedByUser { get; set; }
+
+}
+
+public class AddTaskDto
+{
+    public string Name { get; set; }
+    public int ProjectId { get; set; }
+    public int StatusId { get; set; }
+    public int CreatedByUserId { get; set; }
+
+}
+
+//public class UpdateTaskNameDto
+//{
+//    public string Name { get; set; }
+
+
+//}
+
+//public class UpdateTaskStatusDto
+//{
+//    [AllowedValues([Status.Open, Status.Closed, Status.InProgress])]
+//    public Status Status { get; set; }
+//}
