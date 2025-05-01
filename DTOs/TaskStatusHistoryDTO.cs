1c9d87d8784304c8a241a97f196092dd697ae1a9
Saleh Mohammed <salhbnsmyd3@gmail.com>
2025-05-01 13:57:32 +0300

    add task status dto

---
+++ DTOs/TaskStatusHistoryDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectTaskManagement.API.DTOs;
+
+public class GetTaskStatusHistoryDto
+{
+    public int Id { get; set; }
+    public string TaskName { get; set; }
+    public string ChangedToStatus { get; set; }
+    public string ByUser { get; set; }
+    public DateTime Date { get; set; }
+}
